06577da0d982055ed65b3e7ecdcefa04b413bbbd
Alex Pshonik <alex.pshonik@gmail.com>
2018-11-03 13:05:01 +0200

    Mobile menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,18 @@
 let $ = require('jquery');
-import './full-page'
 import fullPageScroll from './full-page';
 
 $(document).ready(function () {
   fullPageScroll;
+
+  $('.nav-toggle__arrow').on('click', function() {
+    $(this).toggleClass('active');
+    $('.site-nav').toggleClass('toggled');
+  })
+
+  $(window).resize(function () {
+    if(window.innerWidth > 992) {
+      $('.nav-toggle__arrow').removeClass('active');
+      $('.site-nav').removeClass('toggled');
+    }
+  })
 });
